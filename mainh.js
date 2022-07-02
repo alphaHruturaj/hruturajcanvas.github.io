b5ed8b8f000d7a25e25c658fa79323955bbe1ac6
vidhanshu borade <vidhanshu7218555039@gmail.com>
2022-07-02 12:14:49 +0530

    changed the linke o simulator. html file

--- mainh.js
+++ mainh.js
@@ -1,6 +1,5 @@
 let c = document.getElementById("my-canvas");
 let ctx = c.getContext("2d");
-
 let loadImage = (src, callback) => {
     let img = document.createElement("img");
     img.onload = () => callback(img);
